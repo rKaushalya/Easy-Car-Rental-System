34ed56ead0d9d1711de72c80341ece847477cc1c
rKaushalya <ravinduk56@gmail.com>
2023-10-26 11:21:07 +0530

    Driver Section Navigation Done

---
+++ Front_End/controller/navigation/DriverNavigationController.js
@@ -0,0 +1,21 @@
+initiateUI();
+
+function initiateUI() {
+    clearAll();
+    $("#driverLogin").css("display", "block");
+}
+
+function clearAll() {
+    $("#driverLogin,#driverForm").css('display', 'none');
+}
+
+function setView(viewOb) {
+    clearAll();
+    viewOb.css("display", "block");
+    console.log(viewOb.get(0).id);
+}
+
+//bind events
+$("#lnkDriverLogin").click(function () {
+    setView($("#driverForm"));
+});
